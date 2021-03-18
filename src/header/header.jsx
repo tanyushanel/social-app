385d2a8e09f6ee6082b26e7e9517b95ec8d9d811
tanyushanel <tanyushanel@users.noreply.github.com>
2021-03-18 19:42:43 +0300

    init structure

---
+++ src/header/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <div>
+            <img src="assets/images/logo-light.png" />
+        </div>
+    )
+}
+
+export default Header;
